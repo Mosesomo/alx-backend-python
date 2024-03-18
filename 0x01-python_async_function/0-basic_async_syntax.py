44967f9267da3a187597b438d6a39abb5b3a7655
Mosesomo <moseswamboga4@gmail.com>
2024-03-18 22:51:30 +0300

    using paython annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """asynchronous coroutine that takes in an integer argument\
         (max_delay, with a default value of 10) named wait_random\
         that waits for a random delay between 0 and max_delay\
@@ -12,6 +12,6 @@ async def wait_random(max_delay=10):
         seconds and eventually returns it
     """
 
-    delay = random.uniform(0, max_delay)
+    delay = random.random() * max_delay
     await asyncio.sleep(delay)
     return delay
