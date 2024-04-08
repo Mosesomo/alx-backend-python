de03429a70d0114efa850aac417c270029b2cb72
Mosesomo <moseswamboga4@gmail.com>
2024-04-08 17:55:13 +0300

    memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from typing import Dict, Union, Tuple
-from utilis import access_nested_map, get_json
+from utilis import access_nested_map, get_json, memoize
 from unittest.mock import Mock, patch
 
 
@@ -54,3 +54,26 @@ class TestGetJson(unittest.TestCase):
         res = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(res, test_playload)
+
+
+class TestMemoize(unittest.TestCase):
+    '''Testing memoization'''
+    def test_memoize(self):
+        '''Testing memoize'''
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        test_class = TestClass()  
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            res1 = test_class.a_property
+            res2 = test_class.a_property
+            
+            self.assertEqual(res1, 42)
+            self.assertEqual(res2, 42)
+
+            mock_method.assert_called_once()
